a7dbd2296f3155da262acd6b58a6eb5b2ba95824
AlexGechovski <Aleksg00077@gmail.com>
2026-04-26 13:07:51 +0300

    feat(i18n): reconstruct localisation

---
+++ lib/l10n/app_localizations_bg.dart
@@ -0,0 +1,784 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Bulgarian (`bg`).
+class AppLocalizationsBg extends AppLocalizations {
+  AppLocalizationsBg([String locale = 'bg']) : super(locale);
+
+  @override
+  String get appTitle => 'GeoQuest';
+
+  @override
+  String get settings => 'Настройки';
+
+  @override
+  String get language => 'Език';
+
+  @override
+  String get darkMode => 'Тъмен режим';
+
+  @override
+  String get notifications => 'Известия';
+
+  @override
+  String get home => 'Начало';
+
+  @override
+  String get map => 'Карта';
+
+  @override
+  String get leaderboard => 'Класация';
+
+  @override
+  String get profile => 'Профил';
+
+  @override
+  String get signIn => 'Вход';
+
+  @override
+  String get signUp => 'Регистрация';
+
+  @override
+  String get google => 'Продължи с Google';
+
+  @override
+  String get welcomeBack => 'Добре дошъл отново';
+
+  @override
+  String get continueJourney => 'Продължи приключението';
+
+  @override
+  String get createAccount => 'Създай акаунт';
+
+  @override
+  String get startAdventure => 'Започни българското приключение';
+
+  @override
+  String get forgotPassword => 'Забравена парола?';
+
+  @override
+  String get or => 'или';
+
+  @override
+  String get dontHaveAccount => 'Нямаш акаунт? ';
+
+  @override
+  String get alreadyHaveAccount => 'Вече имаш акаунт? ';
+
+  @override
+  String get preferences => 'Предпочитания';
+
+  @override
+  String get permissions => 'Разрешения';
+
+  @override
+  String get account => 'Акаунт';
+
+  @override
+  String get about => 'Информация';
+
+  @override
+  String get selectLanguage => 'Избери език';
+
+  @override
+  String get english => 'Английски';
+
+  @override
+  String get bulgarian => 'Български';
+
+  @override
+  String get locationAccess => 'Достъп до локация';
+
+  @override
+  String get cameraAccess => 'Достъп до камера';
+
+  @override
+  String get notificationsSub => 'Включи push известия';
+
+  @override
+  String get darkModeSub => 'Настрой темата';
+
+  @override
+  String get editProfile => 'Редактирай профил';
+
+  @override
+  String get aboutApp => 'За приложението';
+
+  @override
+  String get exploreMap => 'Разгледай картата';
+
+  @override
+  String get mapView => 'Карта';
+
+  @override
+  String get listView => 'Списък';
+
+  @override
+  String get filters => 'Филтри';
+
+  @override
+  String get applyFilters => 'Приложи филтри';
+
+  @override
+  String get reset => 'Нулирай';
+
+  @override
+  String get difficulty => 'Трудност';
+
+  @override
+  String get category => 'Категория';
+
+  @override
+  String get maxDistance => 'Макс. разстояние';
+
+  @override
+  String get easy => 'лесно';
+
+  @override
+  String get medium => 'средно';
+
+  @override
+  String get hard => 'трудно';
+
+  @override
+  String get skip => 'Пропусни';
+
+  @override
+  String get next => 'Напред';
+
+  @override
+  String get getStarted => 'Започни';
+
+  @override
+  String get joinExplorers => 'Присъедини се към хиляди изследователи';
+
+  @override
+  String get onboardingWelcomeTitle => 'Добре дошъл в\nGeoQuest Bulgaria';
+
+  @override
+  String get onboardingWelcomeBody =>
+      'Откривай невероятни места из България\nи изпълнявай вълнуващи предизвикателства,\nза да печелиш награди';
+
+  @override
+  String get onboardingHowItWorksTitle => 'Как работи';
+
+  @override
+  String get onboardingHowItWorksBody => 'Твоето приключение в 4 лесни стъпки';
+
+  @override
+  String get onboardingStepDiscover => 'Открий';
+
+  @override
+  String get onboardingStepDiscoverBody =>
+      'Намери близки предизвикателства и забележителности';
+
+  @override
+  String get onboardingStepGo => 'Отиди';
+
+  @override
+  String get onboardingStepGoBody => 'Навигирай до локацията';
+
+  @override
+  String get onboardingStepComplete => 'Завърши';
+
+  @override
+  String get onboardingStepCompleteBody => 'Изпълни предизвикателството';
+
+  @override
+  String get onboardingStepRewards => 'Спечели награди';
+
+  @override
+  String get onboardingStepRewardsBody => 'Събирай точки и постижения';
+
+  @override
+  String get onboardingPermissionsTitle => 'Нуждаем се от\nтвоето разрешение';
+
+  @override
+  String get onboardingPermissionsBody =>
+      'За да ти дадем най-доброто изживяване';
+
+  @override
+  String get onboardingLocationAccessBody =>
+      'Намирай предизвикателства около теб и следи напредъка си, докато изследваш България';
+
+  @override
+  String get onboardingCameraAccessBody =>
+      'Снимай, за да завършваш предизвикателства и да споделяш приключенията си';
+
+  @override
+  String get onboardingNotificationsAccessBody =>
+      'Получавай напомняния за предизвикателства, прогрес и награди в реално време';
+
+  @override
+  String get onboardingPrivacyNote =>
+      'Поверителността ти е важна за нас. Използваме тези данни само за\nда подобрим твоето GeoQuest изживяване.';
+
+  @override
+  String get onboardingReadyTitle => 'Готов ли си\nда изследваш?';
+
+  @override
+  String get onboardingReadyBody =>
+      'Твоето приключение из България започва\nсега. Откривай скрити съкровища и създавай\nнезабравими спомени!';
+
+  @override
+  String get authFailed => 'Неуспешна автентикация';
+
+  @override
+  String get authFullNameHint => 'Пълно име';
+
+  @override
+  String get authEmailHint => 'Имейл';
+
+  @override
+  String get authPasswordHint => 'Парола';
+
+  @override
+  String get authPasswordMismatch => 'Паролите не съвпадат';
+
+  @override
+  String get authTerms =>
+      'Продължавайки, се съгласяваш с\nУсловията за ползване и Политиката за поверителност на GeoQuest';
+
+  @override
+  String get welcomeBackShort => 'Добре дошъл,';
+
+  @override
+  String get levelWord => 'Ниво';
+
+  @override
+  String get nextLevel => 'Следващо ниво';
+
+  @override
+  String get totalPoints => 'Общо точки';
+
+  @override
+  String get completed => 'Завършени';
+
+  @override
+  String get badges => 'Постижения';
+
+  @override
+  String get dailyChallenge => 'Дневно предизвикателство';
+
+  @override
+  String get nextAchievement => 'Следващо постижение';
+
+  @override
+  String get nearbyChallenges => 'Близки предизвикателства';
+
+  @override
+  String get viewAll => 'Виж всички';
+
+  @override
+  String get categories => 'Категории';
+
+  @override
+  String get yourProgress => 'Твоят напредък';
+
+  @override
+  String get remaining => 'Остават';
+
+  @override
+  String get completeWord => 'Завършени';
+
+  @override
+  String get keepItGoing => 'Продължавай така!';
+
+  @override
+  String get currentStreak => 'Текуща серия';
+
+  @override
+  String get allRankings => 'Всички позиции';
+
+  @override
+  String get yourCurrentRank => 'Текущо място';
+
+  @override
+  String get keepExploringHigher => 'Продължи да изследваш, за да се изкачиш!';
+
+  @override
+  String get leaderboardSubtitle =>
+      'Състезавай се с други изследователи из България';
+
+  @override
+  String get leaderboardTip =>
+      'Завършвай повече предизвикателства, за да печелиш точки и да се изкачваш в класацията. Класирането се обновява в реално време!';
+
+  @override
+  String get pointsWord => 'точки';
+
+  @override
+  String get settingsAllowLocationServices =>
+      'Разреши услуги за местоположение';
+
+  @override
+  String get settingsAllowCameraAccess => 'Разреши достъп до камера';
+
+  @override
+  String get settingsAllowNotifications => 'Разреши push известия';
+
+  @override
+  String get settingsEditProfileSub => 'Преглед и редакция на профила';
+
+  @override
+  String get settingsSignOut => 'Изход';
+
+  @override
+  String get settingsSignOutSub => 'Край на локалната сесия';
+
+  @override
+  String get settingsBackendSync => 'Backend синхронизация';
+
+  @override
+  String get settingsSyncNow => 'Синхронизирай сега';
+
+  @override
+  String get settingsSyncing => 'Синхронизиране…';
+
+  @override
+  String get settingsNoSyncYet => 'Все още няма синхронизация';
+
+  @override
+  String settingsLastSync(Object value) {
+    return 'Последна синхронизация: $value';
+  }
+
+  @override
+  String settingsLastError(Object value) {
+    return 'Последна грешка: $value';
+  }
+
+  @override
+  String get settingsSyncComplete => 'Синхронизацията завърши';
+
+  @override
+  String get settingsSyncFailed => 'Синхронизацията се провали';
+
+  @override
+  String settingsVersion(Object value) {
+    return 'Версия $value';
+  }
+
+  @override
+  String get challengeStateActive => 'Активно';
+
+  @override
+  String get challengeStateCompleted => 'Завършено';
+
+  @override
+  String get awayLabel => 'Разстояние';
+
+  @override
+  String get pointsLabel => 'Точки';
+
+  @override
+  String get durationLabel => 'Време';
+
+  @override
+  String get distanceLabel => 'Разстояние';
+
+  @override
+  String get stateLabel => 'Състояние';
+
+  @override
+  String get startedLabel => 'Стартирано';
+
+  @override
+  String get aboutThisChallenge => 'За това предизвикателство';
+
+  @override
+  String get rewardLabel => 'Награда';
+
+  @override
+  String rewardFirstStepsPoints(Object points) {
+    return 'Постижение First Steps • +$points т.';
+  }
+
+  @override
+  String get viewCompletedChallenge => 'Виж завършеното предизвикателство';
+
+  @override
+  String get startChallenge => 'Стартирай предизвикателството';
+
+  @override
+  String get completeChallenge => 'Завърши предизвикателството';
+
+  @override
+  String get navigate => 'Навигирай';
+
+  @override
+  String get checkingLocation => 'Проверка на местоположението...';
+
+  @override
+  String get checkLocation => 'Провери местоположението';
+
+  @override
+  String get challengeCompletedTitle => 'Предизвикателството е завършено';
+
+  @override
+  String get challengeCompletedBody =>
+      'Вече приключи това предизвикателство. Детайлите за наградата са налични.';
+
+  @override
+  String get startFirstTitle => 'Първо стартирай';
+
+  @override
+  String get startFirstBody =>
+      'Активирай предизвикателството, за да отключиш навигация и завършване.';
+
+  @override
+  String get navigateLocationTitle => 'Навигирай до мястото';
+
+  @override
+  String get navigateLocationBody =>
+      'Отвори навигацията и завърши предизвикателството, когато пристигнеш.';
+
+  @override
+  String get locationPermissionRequiredTitle =>
+      'Нужно е разрешение за местоположение';
+
+  @override
+  String get locationPermissionRequiredBody =>
+      'Разреши услугите за местоположение, за да потвърдим позицията ти.';
+
+  @override
+  String get tooFarAwayTitle => 'Твърде далеч си';
+
+  @override
+  String tooFarAwayBody(Object value) {
+    return 'Трябва да си в рамките на 100 м. Текущо разстояние: $value.';
+  }
+
+  @override
+  String get locationReachedTitle => 'Локацията е достигната';
+
+  @override
+  String get locationReachedBody =>
+      'Вече можеш да започнеш финалния поток и да изпратиш снимка като доказателство.';
+
+  @override
+  String get challengeStartedTitle => 'Предизвикателството започна!';
+
+  @override
+  String get challengeStartedBody =>
+      'Всичко е готово. Отиди до обекта и завърши предизвикателството, когато пристигнеш.';
+
+  @override
+  String get challengeStartedTip =>
+      'Съвет: първо използвай навигацията. После натисни „Завърши предизвикателството“ в детайлния екран.';
+
+  @override
+  String get startNavigation => 'Старт на навигация';
+
+  @override
+  String get backToMap => 'Назад към картата';
+
+  @override
+  String get photoAcceptedTitle => 'Снимката е приета! 🎉';
+
+  @override
+  String get photoAcceptedBody =>
+      'Страхотна работа! Предизвикателството беше завършено успешно.';
+
+  @override
+  String get firstStepsBadge => 'Постижение First Steps';
+
+  @override
+  String pointsEarned(Object points) {
+    return '+$points спечелени точки';
+  }
+
+  @override
+  String get updatingRewards => 'Обновяване на наградите...';
+
+  @override
+  String get viewRewardDetails => 'Виж детайли за наградата';
+
+  @override
+  String get uploadingProofTitle => 'Качване на доказателство...';
+
+  @override
+  String get uploadingProofBody => 'Моля, изчакай докато потвърдим изпратеното';
+
+  @override
+  String get editProfileTitle => 'Редакция на профила';
+
+  @override
+  String get tapProfilePhoto => 'Докосни профилната снимка, за да я смениш';
+
+  @override
+  String get personalInformation => 'Лична информация';
+
+  @override
+  String get fullName => 'Пълно име';
+
+  @override
+  String get pleaseEnterName => 'Моля, въведи име';
+
+  @override
+  String get emailLabel => 'Имейл';
+
+  @override
+  String get pleaseEnterEmail => 'Моля, въведи имейл';
+
+  @override
+  String get pleaseEnterValidEmail => 'Моля, въведи валиден имейл';
+
+  @override
+  String get bioLabel => 'Био';
+
+  @override
+  String get saving => 'Запазване...';
+
+  @override
+  String get saveChanges => 'Запази промените';
+
+  @override
+  String get choosePhoto => 'Избери снимка';
+
+  @override
+  String get takePhoto => 'Направи снимка';
+
+  @override
+  String get saveProfileError =>
+      'Профилът не можа да се запази. Провери данните си.';
+
+  @override
+  String profileLevelExplorer(Object value) {
+    return 'Изследовател ниво $value';
+  }
+
+  @override
+  String levelValue(Object value) {
+    return 'Ниво $value';
+  }
+
+  @override
+  String pointsProgress(Object points, Object next) {
+    return '$points / $next т.';
+  }
+
+  @override
+  String pointsToLevel(Object points, Object level) {
+    return '$points точки до ниво $level';
+  }
+
+  @override
+  String get inProgress => 'В процес';
+
+  @override
+  String get recentUnlocks => 'Скорошни отключвания';
+
+  @override
+  String get completedChallenges => 'Завършени предизвикателства';
+
+  @override
+  String daysCount(Object value) {
+    return '$value дни';
+  }
+
+  @override
+  String bestCount(Object value) {
+    return 'Най-добро $value';
+  }
+
+  @override
+  String dayStreak(Object value) {
+    return '$value-дневна серия';
+  }
+
+  @override
+  String bestDays(Object value) {
+    return 'Най-добро: $value дни';
+  }
+
+  @override
+  String dailyChallengeDistanceAway(Object value) {
+    return '⌖ $value км разстояние';
+  }
+
+  @override
+  String distanceAway(Object value) {
+    return '$value км разстояние';
+  }
+
+  @override
+  String pointsPts(Object value) {
+    return '+$value т.';
+  }
+
+  @override
+  String challengesCount(Object value) {
+    return '$value предизвикателства';
+  }
+
+  @override
+  String get challengeCompleteCongrats => '🎉 Поздравления!';
+
+  @override
+  String get challengeCompleteSuccess =>
+      'Предизвикателството е завършено успешно!';
+
+  @override
+  String get yourRewards => 'Твоите награди';
+
+  @override
+  String get pointsEarnedLabel => 'Спечелени точки';
+
+  @override
+  String get badgeUnlockedForCompletingChallenge =>
+      'Отключено постижение!\nза завършено предизвикателство';
+
+  @override
+  String get totalCompleted => 'Общо\nзавършени';
+
+  @override
+  String get currentLevel => 'Текущо ниво';
+
+  @override
+  String get continueExploring => '🚀 Продължи да изследваш';
+
+  @override
+  String get reachedNewLevel => '✨ Достигна ново ниво!';
+
+  @override
+  String pointsUntilNextLevel(Object points, Object level) {
+    return '✨ Продължавай страхотно! Остават само $points точки до ниво $level!';
+  }
+
+  @override
+  String get requestingPermissions => 'Искане на разрешения...';
+
+  @override
+  String achievementUnlocked(Object subtitle, Object date) {
+    return '$subtitle\nОтключено $date';
+  }
+
+  @override
+  String get completedCheck => 'Завършено ✓';
+
+  @override
+  String leaderboardLevelCompleted(Object level, Object completed) {
+    return 'Ниво $level  •  $completed завършени';
+  }
+
+  @override
+  String get achievementFirstSteps => 'First Steps';
+
+  @override
+  String get achievementChallengeSeeker => 'Ловец на предизвикателства';
+
+  @override
+  String get achievementNatureExplorer => 'Изследовател на природата';
+
+  @override
+  String get achievementNatureMaster => 'Майстор на природата';
+
+  @override
+  String get achievementCultureLover => 'Любител на културата';
+
+  @override
+  String get achievementHistoryHunter => 'Ловец на история';
+
+  @override
+  String get achievementAdventureAwaits => 'Приключението зове';
+
+  @override
+  String get achievementStreakStarter => 'Старт на серия';
+
+  @override
+  String get achievementExplorer => 'Изследовател';
+
+  @override
+  String get achievementMountainClimber => 'Планински катерач';
+
+  @override
+  String get achievementCompleteFirstChallenge =>
+      'Завърши първото си предизвикателство';
+
+  @override
+  String get achievementCompleteFiveNature =>
+      'Завърши 5 природни предизвикателства';
+
+  @override
+  String get achievementCompleteTwentyFive => 'Завърши 25 предизвикателства';
+
+  @override
+  String get achievementCompleteAllMountain =>
+      'Завърши всички планински предизвикателства';
+
+  @override
+  String get achievementCompleteTen => 'Завърши 10 предизвикателства';
+
+  @override
+  String get achievementCompleteFiveCultural =>
+      'Завърши 5 културни предизвикателства';
+
+  @override
+  String get categoryCultural => 'Културни';
+
+  @override
+  String get categoryNature => 'Природа';
+
+  @override
+  String get categoryHistorical => 'Исторически';
+
+  @override
+  String get categoryAdventure => 'Приключенски';
+
+  @override
+  String get liveCameraPreview => 'Преглед от камера на живо';
+
+  @override
+  String get cameraFallbackPreview =>
+      'Камерата не е налична — резервен преглед';
+
+  @override
+  String get positionLandmark =>
+      'Позиционирай обекта в кадъра и натисни за снимка';
+
+  @override
+  String get confirm => 'Потвърди';
+
+  @override
+  String get retake => 'Заснеми отново';
+
+  @override
+  String get emptyChallengesTitle => 'Няма намерени предизвикателства';
+
+  @override
+  String get emptyChallengesBody =>
+      'Промени филтрите или разгледай друга зона.';
+
+  @override
+  String get emptyInProgressTitle => 'Няма постижения в процес';
+
+  @override
+  String get emptyInProgressBody =>
+      'Започни ново предизвикателство, за да работиш към следващото постижение.';
+
+  @override
+  String get emptyUnlocksTitle => 'Няма скорошни отключвания';
+
+  @override
+  String get emptyUnlocksBody =>
+      'Завършвай предизвикателства, за да отключваш постижения и етапи.';
+
+  @override
+  String get emptyCompletedChallengesTitle =>
+      'Все още няма завършени предизвикателства';
+
+  @override
+  String get emptyCompletedChallengesBody =>
+      'Тук ще се показват приключенията, които си завършил.';
+
+  @override
+  String get emptyLeaderboardTitle => 'Класацията е празна';
+
+  @override
+  String get emptyLeaderboardBody =>
+      'Завърши предизвикателства, за да бъдеш първият изследовател в класацията.';
+}
